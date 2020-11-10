be635ca79d6f82266db91204749d22bb895eba1d
Adam Tyler <mr.adamtyler@gmail.com>
2020-11-10 13:25:45 -0800

    Implement 'edit' route
    Add form to urls_show which POSTs to /urls/:id to allow longURL updates
    Add handler for POST to /urls/:id
    Add 'Edit' column & button to /urls

--- express_server.js
+++ express_server.js
@@ -43,6 +43,11 @@ app.get('/urls/:shortURL', (req, res) => {
   res.render('urls_show', templateVars);
 })  
 
+app.post('/urls/:shortURL', (req, res) => {
+  urlDatabase[req.params.shortURL] = req.body.longURL
+  res.redirect('/urls')
+})
+
 app.post('/urls/:shortURL/delete', (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect('/urls')
