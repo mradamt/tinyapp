b8510bb45a1de880bc617f508445a98228764ee6
Adam Tyler <mr.adamtyler@gmail.com>
2020-11-10 13:22:34 -0800

    Rearrange express_server: group routes' get/post handlers together

--- express_server.js
+++ express_server.js
@@ -31,11 +31,6 @@ app.post('/urls', (req, res) => {
   res.redirect(`/urls/${shortURL}`, )
 })
 
-app.post('/urls/:shortURL/delete', (req, res) => {
-  delete urlDatabase[req.params.shortURL];
-  res.redirect('/urls')
-})
-
 app.get('/urls/new', (req, res) => {
   res.render('urls_new');
 })
@@ -47,6 +42,11 @@ app.get('/urls/:shortURL', (req, res) => {
   };  
   res.render('urls_show', templateVars);
 })  
+
+app.post('/urls/:shortURL/delete', (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect('/urls')
+})
 
 app.get('/u/:shortURL', (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
