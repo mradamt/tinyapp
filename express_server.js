7cd3d7a906c96a1e1762316683938c20a203e221
adam <mr.adamtyler@gmail.com>
2020-11-10 01:35:41 +0000

    Reorder request handlers; add body-parser dependency
    Request handler order to be most specific to least specific
    ... so urls/new above urls/:shortURL
    Also remove dependency in express_server that was auto-added

--- express_server.js
+++ express_server.js
@@ -1,8 +1,10 @@
-const { request } = require("express");
+const bodyParser = require("body-parser");
 
 const express = require('express');
 const app = express();
 const PORT = 8080;
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -16,17 +18,19 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+})
+
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = { 
     shortURL: req.params.shortURL, 
     longURL: urlDatabase[req.params.shortURL]
-  };
+  };  
   res.render('urls_show', templateVars);
-})
+})  
 
-app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
-})
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
