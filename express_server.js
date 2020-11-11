fad1ccb78a9a48851d2b40a3e2451400268e6fa0
Adam Tyler <mr.adamtyler@gmail.com>
2020-11-10 17:20:42 -0800

    Add logout route

--- express_server.js
+++ express_server.js
@@ -25,6 +25,11 @@ app.post('/login', (req, res) =>{
   res.redirect('/urls')
 })
 
+app.post('/logout', (req, res) => {
+  res.clearCookie('username')
+  res.redirect('/urls')
+})
+
 app.get('/urls', (req, res) => {
   const templateVars = { 
     urls: urlDatabase,
