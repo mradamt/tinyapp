2cad63f9460bd65c57b2ce739193c7c08a983cde
Adam Tyler <mr.adamtyler@gmail.com>
2020-11-10 17:03:14 -0800

    Add username to header; add username variable to all pages which <%- include... header %>

--- express_server.js
+++ express_server.js
@@ -26,7 +26,10 @@ app.post('/login', (req, res) =>{
 })
 
 app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"], 
+  };
   res.render('urls_index', templateVars);
 });
 
@@ -40,13 +43,15 @@ app.post('/urls', (req, res) => {
 })
 
 app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
+  const templateVars = { username: req.cookies["username"] }
+  res.render('urls_new', templateVars);
 })
 
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = { 
     shortURL: req.params.shortURL, 
-    longURL: urlDatabase[req.params.shortURL]
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"],
   };  
   res.render('urls_show', templateVars);
 })  
